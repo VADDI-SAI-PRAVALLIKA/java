1c2dff41df07cc49699d106491f15fa0ad0b8f43
VADDI-SAI-PRAVALLIKA <58809821+VADDI-SAI-PRAVALLIKA@users.noreply.github.com>
2020-01-28 15:56:58 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Java
+{
+public static void main(String[] args)
+{
+ Scanner sc=new Scanner(System.in);
+ String s1=sc.nextLine();
+ String s2=sc.nextLine();
+ System.out.println(s1+" Technologies "+s2);
+}
+}
